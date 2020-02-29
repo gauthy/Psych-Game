9322f369706dccc9d61805f78d979aee6b97cdb1
GOWTHAM <NDH00045@Nmcorp.Nissan.Biz>
2020-02-29 12:40:48 +0530

    added in controller

--- game/src/main/java/com/psych/game/controller/HelloController.java
+++ game/src/main/java/com/psych/game/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.psych.game;
+package com.psych.game.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
